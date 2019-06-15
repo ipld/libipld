c69fc4f41ed529a254b2922f07173cc264743891
David Craven <david@craven.ch>
2019-06-15 23:12:23 +0200

    Add RawBlock.

--- src/block.rs
+++ src/block.rs
@@ -3,18 +3,22 @@ use crate::codec::{Codec, ToBytes};
 use crate::hash::Hash;
 use crate::untyped::Ipld;
 use cid::{Cid, Prefix};
+use std::convert::TryFrom;
 use std::marker::PhantomData;
 
-/// Block
+/// Raw block
 #[derive(Clone, Debug, Hash, PartialEq, Eq)]
-pub struct Block<TCodec, THash> {
-    codec: PhantomData<TCodec>,
-    hash: PhantomData<THash>,
+pub struct RawBlock {
     cid: Cid,
     data: Vec<u8>,
 }
 
-impl<TCodec, THash> Block<TCodec, THash> {
+impl RawBlock {
+    /// Creates a new `RawBlock`
+    pub fn new(cid: Cid, data: Vec<u8>) -> Self {
+        RawBlock { cid, data }
+    }
+
     /// Returns the `CID` of the `Block`.
     pub fn cid(&self) -> &Cid {
         &self.cid
@@ -31,6 +35,42 @@ impl<TCodec, THash> Block<TCodec, THash> {
     }
 }
 
+impl<TCodec, THash> From<Block<TCodec, THash>> for RawBlock {
+    fn from(block: Block<TCodec, THash>) -> Self {
+        block.to_raw()
+    }
+}
+
+/// Block
+#[derive(Clone, Debug, Hash, PartialEq, Eq)]
+pub struct Block<TCodec, THash> {
+    codec: PhantomData<TCodec>,
+    hash: PhantomData<THash>,
+    raw: RawBlock,
+}
+
+impl<TCodec, THash> Block<TCodec, THash> {
+    /// Returns the `CID` of the `Block`.
+    pub fn cid(&self) -> &Cid {
+        self.raw.cid()
+    }
+
+    /// Returns the data bytes of the `Block`.
+    pub fn data(&self) -> &Vec<u8> {
+        self.raw.data()
+    }
+
+    /// Takes a block apart.
+    pub fn into(self) -> (Cid, Vec<u8>) {
+        self.raw.into()
+    }
+
+    /// Returns the raw block.
+    pub fn to_raw(self) -> RawBlock {
+        self.raw
+    }
+}
+
 impl<TCodec: Codec + ToBytes, THash: Hash> From<&Ipld> for Block<TCodec, THash> {
     fn from(ipld: &Ipld) -> Self {
         let data = TCodec::to_bytes(ipld);
@@ -44,8 +84,29 @@ impl<TCodec: Codec + ToBytes, THash: Hash> From<&Ipld> for Block<TCodec, THash> 
         Block {
             codec: PhantomData,
             hash: PhantomData,
-            cid,
-            data,
+            raw: RawBlock::new(cid, data),
+        }
+    }
+}
+
+impl<TCodec: Codec, THash: Hash> TryFrom<RawBlock> for Block<TCodec, THash> {
+    type Error = failure::Error;
+
+    fn try_from(raw: RawBlock) -> Result<Self, Self::Error> {
+        let prefix = Prefix {
+            version: TCodec::VERSION,
+            codec: TCodec::CODEC,
+            mh_type: THash::HASH,
+            mh_len: THash::HASH.size() as usize,
+        };
+        if raw.cid().prefix() == prefix {
+            Ok(Block {
+                codec: PhantomData,
+                hash: PhantomData,
+                raw,
+            })
+        } else {
+            Err(failure::format_err!("Prefix doesn't match"))
         }
     }
 }
