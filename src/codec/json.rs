08e6d1a0f818c308f160e8f15e87e4042056448e
David Craven <david@craven.ch>
2019-06-16 11:43:48 +0200

    Implement dag json decode.

--- src/codec/json.rs
+++ src/codec/json.rs
@@ -36,6 +36,47 @@ fn encode(ipld: &Ipld) -> Value {
     }
 }
 
+fn decode(json: &Value) -> Ipld {
+    match json {
+        Value::Null => Ipld::Null(IpldNull),
+        Value::Bool(b) => Ipld::Bool(IpldBool(*b)),
+        Value::Number(num) => {
+            if let Some(i) = num.as_i64() {
+                return Ipld::Integer(IpldInteger::I64(i));
+            }
+            if let Some(i) = num.as_u64() {
+                return Ipld::Integer(IpldInteger::U64(i));
+            }
+            if let Some(f) = num.as_f64() {
+                return Ipld::Float(IpldFloat(f));
+            }
+            Ipld::Null(IpldNull)
+        }
+        Value::String(string) => Ipld::String(IpldString(string.to_owned())),
+        Value::Array(array) => Ipld::List(IpldList(array.iter().map(decode).collect())),
+        Value::Object(object) => {
+            match object.get("/") {
+                Some(Value::String(string)) => {
+                    if let Some(cid) = Cid::from(string.as_str()).ok() {
+                        return Ipld::Link(IpldLink(cid));
+                    }
+                }
+                Some(Value::Object(object)) => {
+                    if let Value::String(string) = &object["base64"] {
+                        let alphabet = Base::Base64.alphabet();
+                        if let Some(bytes) = base_x::decode(alphabet, &string).ok() {
+                            return Ipld::Bytes(IpldBytes(bytes));
+                        }
+                    }
+                }
+                _ => {}
+            }
+            let map = object.iter().map(|(k, v)| (k.to_owned(), decode(v))).collect();
+            Ipld::Map(IpldMap(map))
+        }
+    }
+}
+
 impl Codec for DagJson {
     type Data = serde_json::Value;
 
@@ -46,8 +87,8 @@ impl Codec for DagJson {
         encode(ipld)
     }
 
-    fn decode(_data: &Self::Data) -> Ipld {
-        Ipld::Null(IpldNull)
+    fn decode(data: &Self::Data) -> Ipld {
+        decode(data)
     }
 }
 
@@ -105,5 +146,28 @@ mod tests {
         });
         let json2 = DagJson::encode(&ipld);
         assert_eq!(json, json2);
+    }
+
+    #[test]
+    fn decode_json() {
+        let link = ipld!(null);
+        let ipld = ipld!({
+            "number": 1,
+            "list": [true, null],
+            "bytes": vec![0, 1, 2, 3],
+            "link": json_cid!(link),
+        });
+        let json = json!({
+            "number": 1,
+            "list": [true, null],
+            "bytes": {
+                "/": { "base64": "AQID" },
+            },
+            "link": {
+                "/": json_cid!(link).to_string(),
+            }
+        });
+        let ipld2 = DagJson::decode(&json);
+        assert_eq!(ipld, ipld2);
     }
 }
