46c04b0a296dce0a490277f4ac76fc6183cfc6d0
David Craven <david@craven.ch>
2019-09-09 20:53:38 +0200

    Implement cbor serializer.

--- build.rs
+++
@@ -1,7 +0,0 @@
-fn main() -> Result<(), protoc_rust::Error> {
-    protoc_rust::run(protoc_rust::Args {
-        out_dir: "src/codec/pb/gen",
-        input: &["src/codec/pb/gen/dag_pb.proto"],
-        ..Default::default()
-    })
-}
--- src/codec/cbor.rs
+++ src/codec/cbor.rs
@@ -1,43 +1,200 @@
 //! CBOR codec.
 use crate::codec::Codec;
-use crate::error::Result;
-use crate::ipld::{Ipld, IpldRef};
+use crate::error::{format_err, Result};
+use crate::ipld::{Ipld, IpldKey, IpldRef};
+use byteorder::{BigEndian, ByteOrder};
+use cid::Cid;
+use core::convert::TryInto;
+use std::collections::BTreeMap;
+use std::io::Write;
 
 /// CBOR codec.
 #[derive(Clone, Debug, Hash, PartialEq, Eq)]
 pub struct DagCbor;
 
-/*fn encode(ipld: &Ipld) -> Result<Value> {
-    let cbor = match ipld {
-        Ipld::Null => Value::Null,
-        Ipld::Bool(b) => Value::Bool(*b),
-        Ipld::Integer(i) => Value::Integer(*i),
-        Ipld::Float(f) => Value::Float(*f),
-        Ipld::Bytes(b) => Value::Bytes(b.to_owned()),
-        Ipld::String(s) => Value::Text(s.to_owned()),
-        Ipld::List(l) => {
-            let mut array = Vec::with_capacity(l.len());
-            for item in l.iter() {
-                array.push(encode(item)?);
-            }
-            Value::Array(array)
-        }
-        Ipld::Map(m) => {
-            let mut map = BTreeMap::new();
-            for (k, v) in m {
-                map.insert(encode(&k.to_owned().into())?, encode(v)?);
-            }
-            Value::Map(map)
-        }
-        Ipld::Link(cid) => {
-            let mut map = BTreeMap::new();
-            map.insert(Value::Integer(42), Value::Bytes(cid.to_bytes()));
-            Value::Map(map)
-        }
-    };
-    Ok(cbor)
+struct Encoder<W> {
+    writer: W,
 }
 
+impl<W: Write> Encoder<W> {
+    pub fn new(writer: W) -> Self {
+        Self { writer }
+    }
+
+    fn write_u8(&mut self, major: u8, value: u8) -> Result<()> {
+        if value <= 0x17 {
+            self.writer.write_all(&[major << 5 | value])?;
+        } else {
+            let buf = [major << 5 | 24, value];
+            self.writer.write_all(&buf)?;
+        }
+        Ok(())
+    }
+
+    fn write_u16(&mut self, major: u8, value: u16) -> Result<()> {
+        if value <= u16::from(u8::max_value()) {
+            self.write_u8(major, value as u8)?;
+        } else {
+            let mut buf = [major << 5 | 25, 0, 0];
+            BigEndian::write_u16(&mut buf[1..], value);
+            self.writer.write_all(&buf)?;
+        }
+        Ok(())
+    }
+
+    fn write_u32(&mut self, major: u8, value: u32) -> Result<()> {
+        if value <= u32::from(u16::max_value()) {
+            self.write_u16(major, value as u16)?;
+        } else {
+            let mut buf = [major << 5 | 26, 0, 0, 0, 0];
+            BigEndian::write_u32(&mut buf[1..], value);
+            self.writer.write_all(&buf)?;
+        }
+        Ok(())
+    }
+
+    fn write_u64(&mut self, major: u8, value: u64) -> Result<()> {
+        if value <= u64::from(u32::max_value()) {
+            self.write_u32(major, value as u32)?;
+        } else {
+            let mut buf = [major << 5 | 27, 0, 0, 0, 0, 0, 0, 0, 0];
+            BigEndian::write_u64(&mut buf[1..], value);
+            self.writer.write_all(&buf)?;
+        }
+        Ok(())
+    }
+
+    fn write_tag(&mut self, tag: u64) -> Result<()> {
+        self.write_u64(6, tag)?;
+        Ok(())
+    }
+
+    fn encode_null(&mut self) -> Result<()> {
+        self.writer.write_all(&[0xf6])?;
+        Ok(())
+    }
+
+    fn encode_bool(&mut self, value: bool) -> Result<()> {
+        let value = if value { 0xf5 } else { 0xf4 };
+        self.writer.write_all(&[value])?;
+        Ok(())
+    }
+
+    fn encode_int(&mut self, value: i128) -> Result<()> {
+        if value < 0 {
+            if -(value + 1) > u64::max_value() as i128 {
+                return Err(format_err!("The number can't be stored in CBOR."));
+            }
+            self.write_u64(1, -(value + 1) as u64)?;
+        } else {
+            if value > u64::max_value() as i128 {
+                return Err(format_err!("The number can't be stored in CBOR."));
+            }
+            self.write_u64(0, value as u64)?;
+        }
+        Ok(())
+    }
+
+    fn encode_float(&mut self, _value: f64) -> Result<()> {
+        unimplemented!()
+    }
+
+    fn encode_bytes(&mut self, value: &[u8]) -> Result<()> {
+        self.write_u64(2, value.len() as u64)?;
+        self.writer.write_all(value)?;
+        Ok(())
+    }
+
+    fn encode_str(&mut self, value: &str) -> Result<()> {
+        self.write_u64(3, value.len() as u64)?;
+        self.writer.write_all(value.as_bytes())?;
+        Ok(())
+    }
+
+    fn encode_list(&mut self, value: &[Ipld]) -> Result<()> {
+        self.write_u64(4, value.len().try_into()?)?;
+        for ipld in value {
+            self.encode_ipld(ipld)?;
+        }
+        Ok(())
+    }
+
+    fn encode_list_ref<'a>(&mut self, value: &[IpldRef<'a>]) -> Result<()> {
+        self.write_u64(4, value.len().try_into()?)?;
+        for ipld in value {
+            self.encode_ipld_ref(ipld)?;
+        }
+        Ok(())
+    }
+
+    fn encode_map(&mut self, value: &BTreeMap<IpldKey, Ipld>) -> Result<()> {
+        self.write_u64(5, value.len().try_into()?)?;
+        for (k, v) in value {
+            self.encode_key(k)?;
+            self.encode_ipld(v)?;
+        }
+        Ok(())
+    }
+
+    fn encode_map_ref<'a>(&mut self, value: &BTreeMap<IpldKey, IpldRef<'a>>) -> Result<()> {
+        self.write_u64(5, value.len().try_into()?)?;
+        for (k, v) in value {
+            self.encode_key(k)?;
+            self.encode_ipld_ref(v)?;
+        }
+        Ok(())
+    }
+
+    fn encode_key(&mut self, ipld: &IpldKey) -> Result<()> {
+        match ipld {
+            IpldKey::Integer(i) => self.encode_int(*i),
+            IpldKey::Bytes(b) => self.encode_bytes(b),
+            IpldKey::String(s) => self.encode_str(s),
+        }
+    }
+
+    fn encode_link(&mut self, cid: &Cid) -> Result<()> {
+        self.write_tag(42)?;
+        self.encode_bytes(&cid.to_bytes())?;
+        Ok(())
+    }
+
+    fn encode_ipld(&mut self, ipld: &Ipld) -> Result<()> {
+        match ipld {
+            Ipld::Null => self.encode_null(),
+            Ipld::Bool(b) => self.encode_bool(*b),
+            Ipld::Integer(i) => self.encode_int(*i),
+            Ipld::Float(f) => self.encode_float(*f),
+            Ipld::Bytes(b) => self.encode_bytes(b),
+            Ipld::String(s) => self.encode_str(s),
+            Ipld::List(l) => self.encode_list(l),
+            Ipld::Map(m) => self.encode_map(m),
+            Ipld::Link(c) => self.encode_link(c),
+        }
+    }
+
+    fn encode_ipld_ref<'a>(&mut self, ipld: &IpldRef<'a>) -> Result<()> {
+        match ipld {
+            IpldRef::Null => self.encode_null(),
+            IpldRef::Bool(b) => self.encode_bool(*b),
+            IpldRef::Integer(i) => self.encode_int(*i),
+            IpldRef::Float(f) => self.encode_float(*f),
+            IpldRef::Bytes(b) => self.encode_bytes(b),
+            IpldRef::String(s) => self.encode_str(s),
+            IpldRef::List(l) => self.encode_list(l),
+            IpldRef::OwnedList(l) => self.encode_list_ref(l),
+            IpldRef::Map(m) => self.encode_map(m),
+            IpldRef::OwnedMap(m) => self.encode_map_ref(m),
+            IpldRef::Link(c) => self.encode_link(c),
+        }
+    }
+
+    pub fn encode<'a>(&mut self, ipld: IpldRef<'a>) -> Result<()> {
+        self.encode_ipld_ref(&ipld)
+    }
+}
+
+/*
 fn decode(cbor: &Value) -> Result<Ipld> {
     let ipld = match cbor {
         Value::Null => Ipld::Null,
@@ -73,8 +230,11 @@ impl Codec for DagCbor {
     const VERSION: cid::Version = cid::Version::V1;
     const CODEC: cid::Codec = cid::Codec::DagCBOR;
 
-    fn encode<'a>(_ipld: IpldRef<'a>) -> Result<Box<[u8]>> {
-        Ok(Default::default())
+    fn encode<'a>(ipld: IpldRef<'a>) -> Result<Box<[u8]>> {
+        let mut bytes = Vec::new();
+        let mut enc = Encoder::new(&mut bytes);
+        enc.encode(ipld)?;
+        Ok(bytes.into_boxed_slice())
     }
 
     fn decode(_data: &[u8]) -> Result<Ipld> {
