baa5da64cd7516ce962f233c78bd11409dd14dff
David Craven <david@craven.ch>
2020-02-23 11:46:45 +0100

    Add codec bench.

---
+++ benches/codec.rs
@@ -0,0 +1,30 @@
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use libipld::cbor::DagCborCodec;
+use libipld::cid::Cid;
+use libipld::codec::Codec;
+use libipld::ipld;
+
+fn bench_codec(c: &mut Criterion) {
+    c.bench_function("roundtrip", |b| {
+        let ipld = ipld!({
+          "number": 1,
+          "list": [true, null, false],
+          "bytes": vec![0, 1, 2, 3],
+          "map": { "float": 0.0, "string": "hello" },
+          "link": Cid::random(),
+        });
+        b.iter(|| {
+            let bytes = DagCborCodec::encode(&ipld).unwrap();
+            let ipld2 = DagCborCodec::decode(&bytes).unwrap();
+            black_box(ipld2);
+        });
+    });
+}
+
+criterion_group! {
+    name = codec;
+    config = Criterion::default();
+    targets = bench_codec
+}
+
+criterion_main!(codec);
