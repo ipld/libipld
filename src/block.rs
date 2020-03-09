6230c222967c50304b2d286fb1eed96739bdb98c
David Craven <david@craven.ch>
2020-03-09 14:55:28 +0100

    Use latest cid.

--- src/block.rs
+++ src/block.rs
@@ -11,8 +11,8 @@ pub fn validate(cid: &Cid, data: &[u8]) -> Result<(), BlockError> {
     if data.len() > MAX_BLOCK_SIZE {
         return Err(BlockError::BlockToLarge(data.len()));
     }
-    let hash = digest(cid.hash.algorithm(), &data)?;
-    if hash.as_ref() != cid.hash {
+    let hash = digest(cid.hash().algorithm(), &data)?;
+    if hash.as_ref() != cid.hash() {
         return Err(BlockError::InvalidHash(hash));
     }
     Ok(())
@@ -44,18 +44,18 @@ pub async fn create_cbor_block<H: Hash, C: WriteCbor>(
 
 /// Decode block to ipld.
 pub async fn decode_ipld(cid: &Cid, data: &[u8]) -> Result<Ipld, BlockError> {
-    let ipld = match cid.codec {
+    let ipld = match cid.codec() {
         DagCborCodec::CODEC => DagCborCodec::decode(&data)?,
         cid::Codec::Raw => Ipld::Bytes(data.to_vec()),
-        _ => return Err(BlockError::UnsupportedCodec(cid.codec)),
+        _ => return Err(BlockError::UnsupportedCodec(cid.codec())),
     };
     Ok(ipld)
 }
 
 /// Decode block from cbor.
 pub async fn decode_cbor<C: ReadCbor>(cid: &Cid, mut data: &[u8]) -> Result<C, BlockError> {
-    if cid.codec != DagCborCodec::CODEC {
-        return Err(BlockError::UnsupportedCodec(cid.codec));
+    if cid.codec() != DagCborCodec::CODEC {
+        return Err(BlockError::UnsupportedCodec(cid.codec()));
     }
     let res = C::read_cbor(&mut data)?;
     Ok(res)
