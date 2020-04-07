ea245a1a0492089222a94cc751cd77364f175ad7
Joonas Koivunen <joonas.koivunen@gmail.com>
2020-04-07 21:05:11 +0300

    add: failing roundtrip with cid in cbor

---
+++ dag-cbor/tests/roundtrip.rs
@@ -0,0 +1,24 @@
+use libipld_base::codec::Codec;
+use dag_cbor::DagCborCodec;
+
+#[test]
+fn roundtrip_with_cid() {
+    // generated with go-ipfs
+    // $ echo foobar > file1
+    // $ ipfs add foobar
+    // QmRgutAxd8t7oGkSm4wmeuByG6M51wcTso6cubDdQtuEfL
+    // $ echo -n '{ "foo": { "/": "QmRgutAxd8t7oGkSm4wmeuByG6M51wcTso6cubDdQtuEfL" } }' \
+    //   | ipfs dag put
+    // bafyreibvjvcv745gig4mvqs4hctx4zfkono4rjejm2ta6gtyzkqxfjeily
+    // $ ipfs block get bafyreibvjvcv745gig4mvqs4hctx4zfkono4rjejm2ta6gtyzkqxfjeily \
+    //   | xxd -ps | paste -s --delimiters=
+
+    let input = "a163666f6fd82a582300122031c3d57080d8463a3c63b2923df5a1d40ad7a73eae5a14af584213e5f504ac33";
+    let input = hex::decode(input).unwrap();
+
+    let ipld = DagCborCodec::decode(&input).unwrap();
+
+    let bytes = DagCborCodec::encode(&ipld).unwrap().to_vec();
+
+    assert_eq!(input, bytes);
+}
