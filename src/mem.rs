86ddf78607425687503be2fe38a5e082eaa356a2
David Craven <david@craven.ch>
2020-05-14 23:13:48 +0200

    Implement gc for MemStore.

--- src/mem.rs
+++ src/mem.rs
@@ -1,26 +1,136 @@
 //! Reference implementation of the store traits.
 use crate::cid::Cid;
-use crate::error::StoreError;
+use crate::error::{Error, StoreError};
 use crate::store::{AliasStore, ReadonlyStore, Store, StoreResult, Visibility};
-use async_std::sync::RwLock;
-use std::collections::HashMap;
+use async_std::sync::{Arc, Mutex, MutexGuard, RwLock, RwLockReadGuard, RwLockWriteGuard};
+use async_std::task;
+use std::collections::{HashMap, HashSet};
 
-/// A memory backed store
 #[derive(Default)]
-pub struct MemStore {
-    blocks: RwLock<HashMap<Cid, Box<[u8]>>>,
-    aliases: RwLock<HashMap<Box<[u8]>, Cid>>,
+struct UpgradeableRwLock<T> {
+    lock: Mutex<()>,
+    rwlock: RwLock<T>,
 }
 
-impl ReadonlyStore for MemStore {
+struct UpgradeableRwLockReadGuard<'a, T> {
+    lock: MutexGuard<'a, ()>,
+    guard: RwLockReadGuard<'a, T>,
+}
+
+impl<'a, T> core::ops::Deref for UpgradeableRwLockReadGuard<'a, T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        self.guard.deref()
+    }
+}
+
+struct UpgradeableRwLockWriteGuard<'a, T> {
+    #[allow(unused)]
+    lock: MutexGuard<'a, ()>,
+    guard: RwLockWriteGuard<'a, T>,
+}
+
+impl<'a, T> core::ops::Deref for UpgradeableRwLockWriteGuard<'a, T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        self.guard.deref()
+    }
+}
+
+impl<'a, T> core::ops::DerefMut for UpgradeableRwLockWriteGuard<'a, T> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        self.guard.deref_mut()
+    }
+}
+
+impl<T> UpgradeableRwLock<T> {
+    async fn write(&self) -> UpgradeableRwLockWriteGuard<'_, T> {
+        let lock = self.lock.lock().await;
+        let guard = self.rwlock.write().await;
+        UpgradeableRwLockWriteGuard { lock, guard }
+    }
+
+    async fn read(&self) -> RwLockReadGuard<'_, T> {
+        self.rwlock.read().await
+    }
+
+    async fn upgradeable_read(&self) -> UpgradeableRwLockReadGuard<'_, T> {
+        let lock = self.lock.lock().await;
+        let guard = self.rwlock.read().await;
+        UpgradeableRwLockReadGuard { lock, guard }
+    }
+
+    async fn upgrade<'a>(
+        &'a self,
+        guard: UpgradeableRwLockReadGuard<'a, T>,
+    ) -> UpgradeableRwLockWriteGuard<'a, T> {
+        let UpgradeableRwLockReadGuard { lock, guard } = guard;
+        drop(guard);
+        let guard = self.rwlock.write().await;
+        UpgradeableRwLockWriteGuard { lock, guard }
+    }
+}
+
+#[derive(Default)]
+struct InnerStore {
+    blocks: HashMap<Cid, Box<[u8]>>,
+    roots: HashMap<Cid, usize>,
+}
+
+impl ReadonlyStore for InnerStore {
     fn get<'a>(&'a self, cid: &'a Cid) -> StoreResult<'a, Box<[u8]>> {
         Box::pin(async move {
-            if let Some(data) = self.blocks.read().await.get(cid).cloned() {
+            if let Some(data) = self.blocks.get(cid).cloned() {
                 Ok(data)
             } else {
                 Err(StoreError::BlockNotFound(cid.clone()))
             }
         })
+    }
+}
+
+struct Gc {
+    inner: Arc<UpgradeableRwLock<InnerStore>>,
+}
+
+impl Gc {
+    fn new(store: &MemStore) -> Self {
+        Self {
+            inner: store.inner.clone(),
+        }
+    }
+
+    async fn gc(self) -> Result<(), Error> {
+        // hold read lock on roots
+        let inner = self.inner.upgradeable_read().await;
+
+        let roots: HashSet<Cid> = inner.roots.iter().map(|(k, _)| k.clone()).collect();
+        let live = crate::gc::closure(&*inner, roots).await?;
+        let cids: HashSet<Cid> = inner.blocks.iter().map(|(k, _)| k.clone()).collect();
+
+        // atomically upgrade lock
+        let mut inner = self.inner.upgrade(inner).await;
+
+        // remove dead blocks
+        for cid in cids.difference(&live) {
+            inner.blocks.remove(cid);
+        }
+        Ok(())
+    }
+}
+
+/// A memory backed store
+#[derive(Default)]
+pub struct MemStore {
+    inner: Arc<UpgradeableRwLock<InnerStore>>,
+    aliases: RwLock<HashMap<Box<[u8]>, Cid>>,
+}
+
+impl ReadonlyStore for MemStore {
+    fn get<'a>(&'a self, cid: &'a Cid) -> StoreResult<'a, Box<[u8]>> {
+        Box::pin(async move { self.inner.read().await.get(cid).await })
     }
 }
 
@@ -32,7 +142,10 @@ impl Store for MemStore {
         _visibility: Visibility,
     ) -> StoreResult<'a, ()> {
         Box::pin(async move {
-            self.blocks.write().await.insert(cid.clone(), data);
+            let mut inner = self.inner.write().await;
+            inner.blocks.insert(cid.clone(), data);
+            let count = inner.roots.get(cid).cloned().unwrap_or_default() + 1;
+            inner.roots.insert(cid.clone(), count);
             Ok(())
         })
     }
@@ -45,7 +158,13 @@ impl Store for MemStore {
     /// Marks a block ready for garbage collection.
     fn unpin<'a>(&'a self, cid: &'a Cid) -> StoreResult<'a, ()> {
         Box::pin(async move {
-            self.blocks.write().await.remove(cid);
+            let mut inner = self.inner.write().await;
+            if let Some((cid, count)) = inner.roots.remove_entry(cid) {
+                if count > 1 {
+                    inner.roots.insert(cid, count - 1);
+                }
+            }
+            task::spawn(Gc::new(self).gc());
             Ok(())
         })
     }
@@ -76,5 +195,100 @@ impl AliasStore for MemStore {
 
     fn resolve<'a>(&'a self, alias: &'a [u8]) -> StoreResult<'a, Option<Cid>> {
         Box::pin(async move { Ok(self.aliases.read().await.get(alias).cloned()) })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::block::{decode, encode, Block};
+    use crate::cbor::DagCbor;
+    use crate::cid::Cid;
+    use crate::ipld;
+    use crate::ipld::Ipld;
+    use crate::multihash::Sha2_256;
+    use crate::store::{Store, Visibility};
+    use std::time::Duration;
+
+    async fn get<S: ReadonlyStore>(store: &S, cid: &Cid) -> Option<Ipld> {
+        let bytes = match store.get(cid).await {
+            Ok(bytes) => bytes,
+            Err(StoreError::BlockNotFound { .. }) => return None,
+            Err(e) => Err(e).unwrap(),
+        };
+        Some(decode::<DagCbor, Ipld>(cid, &bytes).unwrap())
+    }
+
+    async fn insert<S: Store>(store: &S, ipld: &Ipld) -> Cid {
+        let Block { cid, data } = encode::<DagCbor, Sha2_256, Ipld>(ipld).unwrap();
+        store.insert(&cid, data, Visibility::Public).await.unwrap();
+        cid
+    }
+
+    #[async_std::test]
+    async fn test_upgrade() {
+        let lock: Arc<UpgradeableRwLock<bool>> = Default::default();
+        let lock2 = lock.clone();
+        let guard = lock.upgradeable_read().await;
+        assert_eq!(*guard, false);
+        task::spawn(async move {
+            let guard = lock2.read().await;
+            assert_eq!(*guard, false);
+            task::sleep(Duration::from_millis(100)).await;
+            assert_eq!(*guard, false);
+            drop(guard);
+            task::sleep(Duration::from_millis(100)).await;
+            let guard = lock2.read().await;
+            assert_eq!(*guard, true);
+        });
+        task::sleep(Duration::from_millis(10)).await;
+        let mut guard = lock.upgrade(guard).await;
+        *guard = true;
+    }
+
+    #[async_std::test]
+    async fn test_gc() {
+        let store = MemStore::default();
+        let a = insert(&store, &ipld!({"a": []})).await;
+        let b = insert(&store, &ipld!({ "b": [&a] })).await;
+        store.unpin(&a).await.unwrap();
+        let c = insert(&store, &ipld!({ "c": [&a] })).await;
+        task::sleep(Duration::from_millis(100)).await;
+        assert!(get(&store, &a).await.is_some());
+        assert!(get(&store, &b).await.is_some());
+        assert!(get(&store, &c).await.is_some());
+        store.unpin(&b).await.unwrap();
+        task::sleep(Duration::from_millis(100)).await;
+        assert!(get(&store, &a).await.is_some());
+        assert!(get(&store, &b).await.is_none());
+        assert!(get(&store, &c).await.is_some());
+        store.unpin(&c).await.unwrap();
+        task::sleep(Duration::from_millis(100)).await;
+        assert!(get(&store, &a).await.is_none());
+        assert!(get(&store, &b).await.is_none());
+        assert!(get(&store, &c).await.is_none());
+    }
+
+    #[async_std::test]
+    async fn test_gc_2() {
+        let store = MemStore::default();
+        let a = insert(&store, &ipld!({"a": []})).await;
+        let b = insert(&store, &ipld!({ "b": [&a] })).await;
+        store.unpin(&a).await.unwrap();
+        let c = insert(&store, &ipld!({ "b": [&a] })).await;
+        task::sleep(Duration::from_millis(100)).await;
+        assert!(get(&store, &a).await.is_some());
+        assert!(get(&store, &b).await.is_some());
+        assert!(get(&store, &c).await.is_some());
+        store.unpin(&b).await.unwrap();
+        task::sleep(Duration::from_millis(100)).await;
+        assert!(get(&store, &a).await.is_some());
+        assert!(get(&store, &b).await.is_some());
+        assert!(get(&store, &c).await.is_some());
+        store.unpin(&c).await.unwrap();
+        task::sleep(Duration::from_millis(100)).await;
+        assert!(get(&store, &a).await.is_none());
+        assert!(get(&store, &b).await.is_none());
+        assert!(get(&store, &c).await.is_none());
     }
 }
