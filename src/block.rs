538ed66240ac4589017cc3fe6f59057483a10c78
David Craven <david@craven.ch>
2020-05-15 22:28:01 +0200

    Add raw decoding functions for encrypted blocks.

--- src/block.rs
+++ src/block.rs
@@ -1,9 +1,9 @@
 //! Block validation
 use crate::cid::Cid;
-use crate::codec::{Codec, Decode, Encode};
+use crate::codec::{Code as CCode, Codec, Decode, Encode};
 use crate::error::{Error, Result};
 use crate::ipld::Ipld;
-use crate::multihash::{Code, Multihasher};
+use crate::multihash::{Code as HCode, Multihasher};
 use crate::raw::Raw;
 use crate::MAX_BLOCK_SIZE;
 #[cfg(feature = "dag-cbor")]
@@ -22,7 +22,7 @@ pub struct Block {
 }
 
 /// Encode a block.
-pub fn encode<C: Codec, H: Multihasher<Code>, E: Encode<C>>(e: &E) -> Result<Block> {
+pub fn encode<C: Codec, H: Multihasher<HCode>, E: Encode<C>>(e: &E) -> Result<Block> {
     let mut data = Vec::new();
     e.encode(&mut data)
         .map_err(|e| Error::CodecError(Box::new(e)))?;
@@ -37,8 +37,18 @@ pub fn encode<C: Codec, H: Multihasher<Code>, E: Encode<C>>(e: &E) -> Result<Blo
     })
 }
 
+/// Raw decode.
+///
+/// Useful for nested encodings when for example the data is encrypted.
+pub fn raw_decode<C: Codec, D: Decode<C>>(codec: CCode, mut data: &[u8]) -> Result<D> {
+    if codec != C::CODE {
+        return Err(Error::UnsupportedCodec(codec));
+    }
+    D::decode(&mut data).map_err(|e| Error::CodecError(Box::new(e)))
+}
+
 /// Decodes a block.
-pub fn decode<C: Codec, D: Decode<C>>(cid: &Cid, mut data: &[u8]) -> Result<D> {
+pub fn decode<C: Codec, D: Decode<C>>(cid: &Cid, data: &[u8]) -> Result<D> {
     if data.len() > MAX_BLOCK_SIZE {
         return Err(Error::BlockTooLarge(data.len()));
     }
@@ -46,19 +56,35 @@ pub fn decode<C: Codec, D: Decode<C>>(cid: &Cid, mut data: &[u8]) -> Result<D> {
     if hash.as_ref() != cid.hash() {
         return Err(Error::InvalidHash(hash));
     }
-    D::decode(&mut data).map_err(|e| Error::CodecError(Box::new(e)))
+    raw_decode(cid.codec(), data)
+}
+
+/// Decode raw ipld.
+///
+/// Useful for nested encodings when for example the data is encrypted.
+pub fn raw_decode_ipld(codec: CCode, data: &[u8]) -> Result<Ipld> {
+    match codec {
+        Raw::CODE => raw_decode::<Raw, _>(codec, data),
+        #[cfg(feature = "dag-cbor")]
+        DagCbor::CODE => raw_decode::<DagCbor, _>(codec, data),
+        #[cfg(feature = "dag-pb")]
+        DagPb::CODE => raw_decode::<DagPb, _>(codec, data),
+        #[cfg(feature = "dag-json")]
+        DagJson::CODE => raw_decode::<DagJson, _>(codec, data),
+        _ => Err(Error::UnsupportedCodec(codec)),
+    }
 }
 
 /// Decode block to ipld.
-pub fn decode_ipld(cid: &Cid, _data: &[u8]) -> Result<Ipld> {
+pub fn decode_ipld(cid: &Cid, data: &[u8]) -> Result<Ipld> {
     match cid.codec() {
-        Raw::CODE => decode::<Raw, _>(cid, _data),
+        Raw::CODE => decode::<Raw, _>(cid, data),
         #[cfg(feature = "dag-cbor")]
-        DagCbor::CODE => decode::<DagCbor, _>(cid, _data),
+        DagCbor::CODE => decode::<DagCbor, _>(cid, data),
         #[cfg(feature = "dag-pb")]
-        DagPb::CODE => decode::<DagPb, _>(cid, _data),
+        DagPb::CODE => decode::<DagPb, _>(cid, data),
         #[cfg(feature = "dag-json")]
-        DagJson::CODE => decode::<DagJson, _>(cid, _data),
+        DagJson::CODE => decode::<DagJson, _>(cid, data),
         _ => Err(Error::UnsupportedCodec(cid.codec())),
     }
 }
