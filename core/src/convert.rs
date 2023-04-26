1161b1fe4fe94718c1a4ddbb711160a11bd32b4e
SionoiS <SionoiS@users.noreply.github.com>
2023-04-26 09:12:36 -0400

    Opss

--- core/src/convert.rs
+++ core/src/convert.rs
@@ -1,10 +1,4 @@
 //! Conversion to and from ipld.
-use crate::{
-    cid::Cid,
-    error::{Error, TypeError, TypeErrorType},
-    ipld::Ipld,
-};
-
 use alloc::{
     borrow::ToOwned,
     boxed::Box,
@@ -12,6 +6,11 @@ use alloc::{
     string::{String, ToString},
     vec::Vec,
 };
+
+use crate::{cid::Cid, ipld::Ipld};
+
+#[cfg(feature = "std")]
+use crate::error::{Error, TypeError, TypeErrorType};
 
 #[cfg(feature = "std")]
 impl TryFrom<Ipld> for () {
@@ -114,46 +113,88 @@ derive_into_ipld!(Map, BTreeMap<String, Ipld>, to_owned);
 derive_into_ipld!(Link, Cid, clone);
 derive_into_ipld!(Link, &Cid, to_owned);
 
+#[cfg(feature = "std")]
 derive_try_from_ipld!(Bool, bool);
+#[cfg(feature = "std")]
 derive_try_from_ipld!(Integer, i8);
+#[cfg(feature = "std")]
 derive_try_from_ipld!(Integer, i16);
+#[cfg(feature = "std")]
 derive_try_from_ipld!(Integer, i32);
+#[cfg(feature = "std")]
 derive_try_from_ipld!(Integer, i64);
+#[cfg(feature = "std")]
 derive_try_from_ipld!(Integer, i128);
+#[cfg(feature = "std")]
 derive_try_from_ipld!(Integer, isize);
+#[cfg(feature = "std")]
 derive_try_from_ipld!(Integer, u8);
+#[cfg(feature = "std")]
 derive_try_from_ipld!(Integer, u16);
+#[cfg(feature = "std")]
 derive_try_from_ipld!(Integer, u32);
+#[cfg(feature = "std")]
 derive_try_from_ipld!(Integer, u64);
+#[cfg(feature = "std")]
 derive_try_from_ipld!(Integer, u128);
+#[cfg(feature = "std")]
 derive_try_from_ipld!(Integer, usize);
+
 //derive_from_ipld!(Float, f32); // User explicit conversion is prefered. Would implicitly lossily convert from f64.
+
+#[cfg(feature = "std")]
 derive_try_from_ipld!(Float, f64);
+#[cfg(feature = "std")]
 derive_try_from_ipld!(String, String);
+#[cfg(feature = "std")]
 derive_try_from_ipld!(Bytes, Vec<u8>);
+#[cfg(feature = "std")]
 derive_try_from_ipld!(List, Vec<Ipld>);
+#[cfg(feature = "std")]
 derive_try_from_ipld!(Map, BTreeMap<String, Ipld>);
+#[cfg(feature = "std")]
 derive_try_from_ipld!(Link, Cid);
 
+#[cfg(feature = "std")]
 derive_try_from_ipld_option!(Bool, bool);
+#[cfg(feature = "std")]
 derive_try_from_ipld_option!(Integer, i8);
+#[cfg(feature = "std")]
 derive_try_from_ipld_option!(Integer, i16);
+#[cfg(feature = "std")]
 derive_try_from_ipld_option!(Integer, i32);
+#[cfg(feature = "std")]
 derive_try_from_ipld_option!(Integer, i64);
+#[cfg(feature = "std")]
 derive_try_from_ipld_option!(Integer, i128);
+#[cfg(feature = "std")]
 derive_try_from_ipld_option!(Integer, isize);
+#[cfg(feature = "std")]
 derive_try_from_ipld_option!(Integer, u8);
+#[cfg(feature = "std")]
 derive_try_from_ipld_option!(Integer, u16);
+#[cfg(feature = "std")]
 derive_try_from_ipld_option!(Integer, u32);
+#[cfg(feature = "std")]
 derive_try_from_ipld_option!(Integer, u64);
+#[cfg(feature = "std")]
 derive_try_from_ipld_option!(Integer, u128);
+#[cfg(feature = "std")]
 derive_try_from_ipld_option!(Integer, usize);
+
 //derive_from_ipld_option!(Float, f32); // User explicit conversion is prefered. Would implicitly lossily convert from f64.
+
+#[cfg(feature = "std")]
 derive_try_from_ipld_option!(Float, f64);
+#[cfg(feature = "std")]
 derive_try_from_ipld_option!(String, String);
+#[cfg(feature = "std")]
 derive_try_from_ipld_option!(Bytes, Vec<u8>);
+#[cfg(feature = "std")]
 derive_try_from_ipld_option!(List, Vec<Ipld>);
+#[cfg(feature = "std")]
 derive_try_from_ipld_option!(Map, BTreeMap<String, Ipld>);
+#[cfg(feature = "std")]
 derive_try_from_ipld_option!(Link, Cid);
 
 #[cfg(test)]
